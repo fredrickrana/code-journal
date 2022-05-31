d8bfff38e08e7e39e7982a88bf569c51272c4fc8
Fredrick Rana <fredrickrana01@gmail.com>
2022-05-31 18:53:15 +0000

    added div[data-view="entry-form"] and a form for creating a journal entry. made all inputs of the journal entry form required. created an event listerner that updates the src attribute of the photo preview when the input value changes.

--- js/main.js
+++ js/main.js
@@ -16,10 +16,27 @@
 // // Use className to change src attribute for the photoURL
 // // Use getAttribute src attribute
 
-var $url = document.getElementById('photo-url');
-var $image = document.querySelector('.image-blank');
-$url.addEventListener('input', photoURL);
+// The goal is to update the src attribute when a url is placed into the photoUrl.
+// We would have to listen for any 'input'events (when the input value changes).
+// assign a variable with the following: a document method that (in this case since we're using labels) takes the ID of the photo-url.
+
+// USE var url = document.getElementByID('photo-url');
+
+// url.addEventListener('input', funtion name)
+// create a function that sets the attribute (updates src) of the img element when the input id='photo-url' changes value
+// in order to do that, we have to take the img src (class=image-blank) and utilize DOM query and assign to a variable
+
+// USE var image = document.querySelector('.image-blank');
+
+// in a function it would be...
+// function photoURL(event) {
+//  image.setAttribute('src', url.value);
+// }
+
+var url = document.getElementById('photo-url');
+var image = document.querySelector('.image-blank');
+url.addEventListener('input', photoURL);
 
 function photoURL(event) {
-  $image.setAttribute('src', $url.value);
+  image.setAttribute('src', url.value);
 }
