0ad177babbc86fd17822eefbd5951bbc768b44c6
Fredrick Rana <fredrickrana01@gmail.com>
2022-06-01 23:36:30 +0000

    completed issue-1-create-an-entry. verified that new entries are saved to local storage along with the rest of the data model on page reload. verified that each new entry receives an incrementing entryId. put the form's input values into a new object, prepend the new object to the entries in the data model, and reset the image preview's src attribute and form inputs. tested functionality of page.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousEntriesJSON = localStorage.getItem('code-journal-entries');
+if (previousEntriesJSON !== null) {
+  data = JSON.parse(previousEntriesJSON);
+}
+
+function storage(event) {
+  var entriesJSON = JSON.stringify(data);
+  localStorage.setItem('code-journal-entries', entriesJSON);
+}
+
+window.addEventListener('beforeunload', storage);
--- js/main.js
+++ js/main.js
@@ -1,42 +1,28 @@
+/* global data */
 
-// review @media pokemon assignment to change the flex-basis when expanding the screen.
-// use addEventListener for 'input' events on the
-
-// document.addEventListener('submit', newEntry);
-
-// function newEntry(event) {
-// var newData = {};
-// var
-// }
-
-// // add a form for creating a journal entry to the div[data-view]='entry-form'
-
-// // edit the form so it looks nice on mobile phone and monitors
-
-// // Use className to change src attribute for the photoURL
-// // Use getAttribute src attribute
-
-// The goal is to update the src attribute when a url is placed into the photoUrl.
-// We would have to listen for any 'input'events (when the input value changes).
-// assign a variable with the following: a document method that (in this case since we're using labels) takes the ID of the photo-url.
-
-// USE var url = document.getElementByID('photo-url');
-
-// url.addEventListener('input', funtion name)
-// create a function that sets the attribute (updates src) of the img element when the input id='photo-url' changes value
-// in order to do that, we have to take the img src (class=image-blank) and utilize DOM query and assign to a variable
-
-// USE var image = document.querySelector('.image-blank');
-
-// in a function it would be...
-// function photoURL(event) {
-//  image.setAttribute('src', url.value);
-// }
-
-var url = document.getElementById('photo-url');
+var url = document.getElementById('photourl');
 var image = document.querySelector('.image-blank');
 url.addEventListener('input', photoURL);
 
 function photoURL(event) {
   image.setAttribute('src', url.value);
+}
+
+var form = document.querySelector('form');
+form.addEventListener('submit', getDataEntry);
+
+function getDataEntry(event) {
+  event.preventDefault();
+  var $titleInfo = document.querySelector('input').value;
+  var $photoURL = document.querySelector('input[id="photourl"]').value;
+  var $notes = document.querySelector('textarea').value;
+  var objectData = {
+    title: $titleInfo,
+    photourl: $photoURL,
+    notes: $notes,
+    entryId: data.nextEntryId++
+  };
+  data.entries.push(objectData);
+  image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  form.reset();
 }
